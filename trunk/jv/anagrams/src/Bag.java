fdaebe96666cc6c27c8c949dc4f62f8cabc37a81
eric.hanchrow <eric.hanchrow@ce75a14a-3c2d-0410-9aa1-cbafc6a0a5bf>
2007-04-07 07:29:20 +0000

    'taint elegant, but we got a bag constructor.
    git-svn-id: http://polyglot-anagrams.googlecode.com/svn@38 ce75a14a-3c2d-0410-9aa1-cbafc6a0a5bf

--- trunk/jv/anagrams/src/Bag.java
+++ trunk/jv/anagrams/src/Bag.java
@@ -12,14 +12,48 @@
  * @author Eric
  */
 public class Bag {
-    private String guts;
+    private static java.math.BigInteger primes[] = {new java.math.BigInteger("2"),
+    new java.math.BigInteger("3"),
+    new java.math.BigInteger("5"),
+    new java.math.BigInteger("7"),
+    new java.math.BigInteger("11"),
+    new java.math.BigInteger("13"),
+    new java.math.BigInteger("17"),
+    new java.math.BigInteger("19"),
+    new java.math.BigInteger("23"),
+    new java.math.BigInteger("29"),
+    new java.math.BigInteger("31"),
+    new java.math.BigInteger("37"),
+    new java.math.BigInteger("41"),
+    new java.math.BigInteger("43"),
+    new java.math.BigInteger("47"),
+    new java.math.BigInteger("53"),
+    new java.math.BigInteger("59"),
+    new java.math.BigInteger("61"),
+    new java.math.BigInteger("67"),
+    new java.math.BigInteger("71"),
+    new java.math.BigInteger("73"),
+    new java.math.BigInteger("79"),
+    new java.math.BigInteger("83"),
+    new java.math.BigInteger("89"),
+    new java.math.BigInteger("97"),
+    new java.math.BigInteger("101")};
+    private java.math.BigInteger guts;
     /** Creates a new instance of Bag */
     public Bag(String s) {
-        guts = s;
+        s = s.toLowerCase();
+        guts = java.math.BigInteger.ONE;
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c >= 'a' && c <= 'z') {
+                guts = guts.multiply(primes[c - 'a']);
+            }
+            
+        }
     }
     
     public Boolean empty() {
-        return guts.length() == 0;
+        return guts == java.math.BigInteger.ONE;
     }
     
 }
