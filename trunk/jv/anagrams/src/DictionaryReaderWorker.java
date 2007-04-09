779bc06bcf3342c99d26c02f543bd72f8a6cf740
eric.hanchrow <eric.hanchrow@ce75a14a-3c2d-0410-9aa1-cbafc6a0a5bf>
2007-04-09 17:02:58 +0000

    restore another piece of -r60: Actually compute the final dictionary list, and store it in DictionaryWorker.rv
    git-svn-id: http://polyglot-anagrams.googlecode.com/svn@63 ce75a14a-3c2d-0410-9aa1-cbafc6a0a5bf

--- trunk/jv/anagrams/src/DictionaryReaderWorker.java
+++ trunk/jv/anagrams/src/DictionaryReaderWorker.java
@@ -16,10 +16,11 @@ import java.util.Vector;
  *
  * @author Eric
  */
-public class DictionaryReaderWorker extends SwingWorker<Object, Void> {
+public class DictionaryReaderWorker extends SwingWorker<Vector<Vector<Object>>, Void> {
     public Vector<Vector<Object>> rv ;
+
     @Override
-    public String doInBackground() {
+    public Vector<Vector<Object>> doInBackground() {
         try {
             // TODO -- Rather than hard-coding this, use a FileOpen
             // dialog to get it; then cache the content somewhere.
@@ -28,7 +29,7 @@ public class DictionaryReaderWorker extends SwingWorker<Object, Void> {
             // computer.
             java.io.BufferedReader in
                     = new java.io.BufferedReader(new java.io.FileReader(System.getProperty("user.home") + "/doodles/anagrams/words"));
-            
+
             setProgress(0);
             // We'll loop twice: once to read the word file into a simple list, and again to convert the list into a hash table.
             // Since the first loop is fast and the second slow, this lets us initialize a progress bar for the second loop,
@@ -36,7 +37,7 @@ public class DictionaryReaderWorker extends SwingWorker<Object, Void> {
             Vector<String> words_from_file = new Vector<String>();
             {
                 String line;
-                
+
                 while ((line = in.readLine()) != null) {
                     line = line.toLowerCase();
                     if (line.length() > 1 || line == "i" || line == "a"){
@@ -52,32 +53,44 @@ public class DictionaryReaderWorker extends SwingWorker<Object, Void> {
             int words_examined = 0;
             for (Iterator it = words_from_file.iterator(); it.hasNext();) {
                 String line = (String) it.next();
-                
+
                 Bag linebag = new Bag(line);
                 Vector<String> existing = NewJFrame.ht.get(linebag);
                 if (existing == null)
                     existing = new Vector<String>();
-                
-                
+
+
                 if (!existing.contains(line))
                     existing.add(line);
                 NewJFrame.ht.put(linebag, existing);
-                
+
+
                 setProgress(++words_examined * 100 / words_from_file.size());
             }
-            
-        } catch (Exception ex) {
+            rv = new Vector<Vector<Object>>();
+            for (Enumeration<Bag> e = NewJFrame.ht.keys(); e.hasMoreElements();) {
+                Bag bag = e.nextElement();
+                Vector<String> words = NewJFrame.ht.get(bag);
+                Vector<Object> one_entry = new Vector<Object>();
+                one_entry.add(bag);
+                one_entry.add(words);
+                rv.add(one_entry);
+            }
+        }
+
+        catch (Exception ex) {
             ex.printStackTrace();
         }
-        return null;
+        return rv;
     }
-    
+
+
     @Override
     protected void done() {
-        
+
     }
     /** Creates a new instance of DictionaryReaderWorker */
     public DictionaryReaderWorker() {
     }
-    
+
 }
