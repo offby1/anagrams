04b6577e00054ff8316b5a5ee8078792e3158591
eric.hanchrow <eric.hanchrow@ce75a14a-3c2d-0410-9aa1-cbafc6a0a5bf>
2007-04-15 23:03:57 +0000

    git-svn-id: http://polyglot-anagrams.googlecode.com/svn@96 ce75a14a-3c2d-0410-9aa1-cbafc6a0a5bf

---
+++ trunk/c++-vs2005/AnniesGram/Form1.h
@@ -0,0 +1,107 @@
+#pragma once
+
+
+namespace AnniesGram {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// Summary for Form1
+	///
+	/// WARNING: If you change the name of this class, you will need to change the
+	///          'Resource File Name' property for the managed resource compiler tool
+	///          associated with all .resx files this class depends on.  Otherwise,
+	///          the designers will not be able to interact properly with localized
+	///          resources associated with this form.
+	/// </summary>
+	public ref class Form1 : public System::Windows::Forms::Form
+	{
+	public:
+		Form1(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: Add the constructor code here
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		~Form1()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+	private: System::Windows::Forms::TextBox^  input;
+	private: System::Windows::Forms::ListView^  output;
+
+	protected: 
+
+	protected: 
+
+	protected: 
+
+
+	private:
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		System::ComponentModel::Container ^components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			this->input = (gcnew System::Windows::Forms::TextBox());
+			this->output = (gcnew System::Windows::Forms::ListView());
+			this->SuspendLayout();
+			// 
+			// input
+			// 
+			this->input->Anchor = static_cast<System::Windows::Forms::AnchorStyles>(((System::Windows::Forms::AnchorStyles::Bottom | System::Windows::Forms::AnchorStyles::Left) 
+				| System::Windows::Forms::AnchorStyles::Right));
+			this->input->Location = System::Drawing::Point(12, 241);
+			this->input->Name = L"input";
+			this->input->Size = System::Drawing::Size(268, 20);
+			this->input->TabIndex = 0;
+			// 
+			// output
+			// 
+			this->output->Anchor = static_cast<System::Windows::Forms::AnchorStyles>((((System::Windows::Forms::AnchorStyles::Top | System::Windows::Forms::AnchorStyles::Bottom) 
+				| System::Windows::Forms::AnchorStyles::Left) 
+				| System::Windows::Forms::AnchorStyles::Right));
+			this->output->Location = System::Drawing::Point(12, 12);
+			this->output->Name = L"output";
+			this->output->Size = System::Drawing::Size(268, 223);
+			this->output->TabIndex = 1;
+			this->output->UseCompatibleStateImageBehavior = false;
+			// 
+			// Form1
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(292, 273);
+			this->Controls->Add(this->output);
+			this->Controls->Add(this->input);
+			this->Name = L"Form1";
+			this->Text = L"Form1";
+			this->ResumeLayout(false);
+			this->PerformLayout();
+
+		}
+#pragma endregion
+	};
+}
+
