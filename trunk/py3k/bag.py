a8c6b6b6d4f23099da5c4d8449fc72bed9455e36
eric.hanchrow <eric.hanchrow@ce75a14a-3c2d-0410-9aa1-cbafc6a0a5bf>
2008-01-12 18:08:37 +0000

    Seems to work!
    git-svn-id: http://polyglot-anagrams.googlecode.com/svn@156 ce75a14a-3c2d-0410-9aa1-cbafc6a0a5bf

---
+++ trunk/py3k/bag.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+
+import string
+import sys
+
+def bag_empty (b):
+    return b == 1
+
+def bag (str):
+    str = str.lower ()
+    primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101]
+    rv = 1
+
+    for c in str:
+        if (c >= 'a') and (c <= 'z'):
+            rv *= primes [ord (c) - ord ('a')]
+
+    return rv
+
+def bags_equal (s1, s2):
+    return s1 == s2
+
+def subtract_bags (b1, b2):
+    quotient  = b1 // b2
+    remainder = b1 % b2
+    if (0 == remainder):
+        return quotient
+    else:
+        return 0
+
+assert (bag_empty (bag ("")))
+
+assert (not (bag_empty (bag ("a"))))
+
+assert (bags_equal (bag ("abc"),
+                    bag ("cba")))
+
+assert (not (bags_equal (bag ("abc"),
+                         bag ("bc"))))
+
+assert (bags_equal (bag ("a"),
+                      subtract_bags (bag("ab"),
+                                     bag ("b"))))
+assert (not (subtract_bags (bag ("a"),
+                            bag ("b"))))
+
+assert (not (subtract_bags (bag ("a"),
+                            bag ("aa"))))
+
+silly_long_string = "When first I was a wee, wee lad\n\
+Eating on my horse\n\
+I had to take a farting duck\n\
+Much to my remorse.\n\
+Oh Sally can't you hear my plea\n\
+When Roe V Wade is nigh\n\
+And candles slide the misty morn\n\
+With dimples on your tie."
+
+ever_so_slightly_longer_string = silly_long_string + "x"
+
+assert (bags_equal (bag ("x"),
+                    subtract_bags (bag (ever_so_slightly_longer_string),
+                                   bag (silly_long_string))))
+
+assert (bags_equal (bag ("abc"),
+                    bag ("ABC")))
+
+print (__name__, "tests passed.", file=sys.stderr)
---
+++ trunk/py3k/dict.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+
+from io import StringIO
+import codecs
+import string
+import re
+import sys
+import pickle
+import os
+from stat import *
+from bag import bag, bag_empty, bags_equal, subtract_bags
+
+has_a_vowel_re = re.compile (r'[aeiou]')
+long_enough_re = re.compile (r'^i$|^a$|^..')
+non_letter_re = re.compile (r'[^a-zA-Z]')
+
+def snarf_dictionary_from_IO (I):
+    print ("Snarfing", I, file=sys.stderr)
+    hash_table = {}
+    for w in re.findall (r'.+', I.read ()):
+        w = w.lower ()
+        if non_letter_re.search (w):
+            continue
+        if (not long_enough_re.match (w)):
+            continue
+        if (not has_a_vowel_re.search (w)):
+            continue
+
+        key = bag(w)
+        if key in hash_table:
+            if (0 == hash_table[key].count (w)): # avoid duplicates
+                hash_table[key].append (w)
+        else:
+            hash_table[key] = [w]
+
+    print ("done", file=sys.stderr)
+    return hash_table
+
+hash_cache = "hash.cache"
+
+def snarf_dictionary (fn):
+    try:
+        fh = open (hash_cache, "rb")
+        rv= pickle.load (fh)
+        print ("Reading cache", hash_cache, "instead of dictionary", fn, file=sys.stderr)
+    except:
+        fh = codecs.open (fn, "r", encoding='utf-8')
+        rv = snarf_dictionary_from_IO (fh)
+        fh.close ()
+        fh = open (hash_cache, "wb")
+        pickle.dump (rv, fh, 2)
+
+    fh.close ()
+    return rv
+
+
+fake_input = "cat\ntac\nfred\n"
+fake_dict = snarf_dictionary_from_IO (StringIO (fake_input))
+
+assert (2 == len (fake_dict.keys ()))
+cat_hits = fake_dict[bag ("cat")]
+assert (2 == len (cat_hits))
+assert (cat_hits[0] == "cat")
+assert (cat_hits[1] == "tac")
+assert (1 == len (fake_dict[bag ("fred")]))
+assert (fake_dict[bag ("fred")][0] == "fred")
+
+print  (__name__, "tests passed.", file=sys.stderr)
