1a20c00b62fd4eac98034565c664301702318fc6
eric.hanchrow <eric.hanchrow@ce75a14a-3c2d-0410-9aa1-cbafc6a0a5bf>
2008-01-31 16:19:18 +0000

    * bag: downcase input * go.sh: make it do something interesting
    git-svn-id: http://polyglot-anagrams.googlecode.com/svn@179 ce75a14a-3c2d-0410-9aa1-cbafc6a0a5bf

--- trunk/arc/go.sh
+++ trunk/arc/go.sh
@@ -1,4 +1,13 @@
 #!/bin/sh
 
 arc=/usr/local/src/langs/arc
-(cat bag.arc dict.arc; echo '(quit)') | (cd $arc/arc0; $arc/plt/bin/mzscheme -m -f as.scm) 
+(
+    cat bag.arc dict.arc anagrams.arc
+    echo '(anagrams (bag "scate") dictionary*)'
+    echo '(quit)'
+    ) \
+        | \
+        (
+    cd $arc/arc0
+    $arc/plt/bin/mzscheme -m -f as.scm
+    ) 
