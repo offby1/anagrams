8f7feef58a8bfd3e49443d195b5385ff290d3285
eric.hanchrow <eric.hanchrow@ce75a14a-3c2d-0410-9aa1-cbafc6a0a5bf>
2008-06-19 17:57:41 +0000

    begin to filter dict entries
    git-svn-id: http://polyglot-anagrams.googlecode.com/svn@200 ce75a14a-3c2d-0410-9aa1-cbafc6a0a5bf

--- trunk/erlang/wheedledict.erl
+++ trunk/erlang/wheedledict.erl
@@ -4,7 +4,7 @@
 
 snarf ()->
     {ok, S} = file:open ("/usr/share/dict/words", read),
-    Lines = more (S, []),
+    Lines = more (S, fun (_Candidate) -> true end, []),
     file:close (S),
     io:format ("Read ~p lines.~n", [length (Lines)]),
     OFN = "snarfage",
@@ -13,10 +13,14 @@ snarf ()->
     io:format ("Wrote to ~p~n", [OFN]),
     file:close (W).
 
-more (S, SoFar)->
+more (S, Criterion, SoFar)->
     case io:get_line (S, '') of
         eof ->
             lists:reverse (SoFar);
         Line ->
-            more (S, [{bag (Line), Line}|SoFar])
+            case  Criterion (Line) of
+                true  -> more (S, Criterion, [{bag (Line), Line}|SoFar]);
+                false -> more (S, Criterion, [SoFar])
+            end
+
     end.
