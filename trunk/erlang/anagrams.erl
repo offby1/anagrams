9ac69b59f8d1115cf7f2ca7f1b02c45d2978bb31
eric.hanchrow <eric.hanchrow@ce75a14a-3c2d-0410-9aa1-cbafc6a0a5bf>
2008-06-19 20:34:34 +0000

    Use qualified names, for no particularly good reason
    git-svn-id: http://polyglot-anagrams.googlecode.com/svn@207 ce75a14a-3c2d-0410-9aa1-cbafc6a0a5bf

--- trunk/erlang/anagrams.erl
+++ trunk/erlang/anagrams.erl
@@ -1,12 +1,10 @@
 -module (anagrams).
 -compile (export_all).
--import (bag, [subtract/2]).
--import (wheedledict, [snarf/0]).
 
 filter (_Bag, [])->
     [];
 filter (Bag, [{Candidate, Words}|T]) ->
-    case subtract (Candidate, Bag) of 
+    case bag:subtract (Candidate, Bag) of 
         0 ->
             filter (Bag, T);
         _ ->
@@ -17,7 +15,7 @@ main ([])->
     io:format ("Dude.  How 'bout an argument?~n");
 main ([CriterionString])->
     Filtered = filter (bag:bag (CriterionString), 
-                       snarf ()),
+                       wheedledict:snarf ()),
     io:format ("Dictionary has ~p words that include ~p.~n", [length (Filtered), CriterionString]);
 main ([CriterionString|Crap]) ->
     io:format ("(ignoring ~p ...)", [Crap]),
