3283fe15cb8d639c76bc061e26e61f93c15a8204
eric.hanchrow <eric.hanchrow@ce75a14a-3c2d-0410-9aa1-cbafc6a0a5bf>
2008-06-20 01:11:16 +0000

    The general idea, but dicts aren't working:         lists:keysearch(bag:bag ("dog"), 1, wheedledict:snarf ()). =>         {value,{5593,"god"}} but it should be         {value,{5593,"god", "dog"}}
    git-svn-id: http://polyglot-anagrams.googlecode.com/svn@209 ce75a14a-3c2d-0410-9aa1-cbafc6a0a5bf

--- trunk/erlang/anagrams.erl
+++ trunk/erlang/anagrams.erl
@@ -10,16 +10,48 @@ filter (Bag, [{Candidate, Words}|T]) ->
         _ ->
             [{Candidate, Words} | filter (Bag, T)]
     end.
-    
+
+flatmap (Func, List) ->
+    {_List2, Acc1} = lists:mapfoldl (fun (Item, Accum)-> {Item, Func (Item) ++ Accum} end, [], List),
+    Acc1.
+
+combine (Words, Anagrams) ->
+    flatmap(fun (W) ->
+                    lists:map (fun (An) -> [W | An] end, Anagrams) end,
+            Words).
+
+anagrams (_Bag, [], _Accum) ->
+    [];
+anagrams (Bag, [OneEntry|RestOfDict] = Dict, Accum)->
+    {ThisKey, TheseWords} = OneEntry,
+    SmallerBag = bag:subtract (Bag, ThisKey),
+    case SmallerBag of
+        0 ->
+            anagrams (Bag, RestOfDict, Accum);
+        1 ->
+            anagrams (Bag, RestOfDict, Accum ++ [[W] || W <- TheseWords]);
+        _ ->
+            anagrams (Bag, RestOfDict, Accum ++
+                      combine (TheseWords,
+                               anagrams (SmallerBag, 
+                                         filter (SmallerBag, Dict),
+                                         Accum)))
+    end.
+
+tinydict () ->
+    [
+    {bag:bag ("dog"), "dog", "god"}
+    ].
+
 main ([])->
     io:format ("Dude.  How 'bout an argument?~n");
 main ([CriterionString])->
-    Filtered = filter (bag:bag (CriterionString), 
+    B = bag:bag (CriterionString),
+    Filtered = filter (B, 
                        wheedledict:snarf ()),
-    io:format ("Dictionary has ~p words that include ~p: ~p.~n",
-               [length (Filtered),
-                CriterionString,
-                Filtered]);
+    io:format ("Dictionary has ~p words that include ~p.~n",
+               [length (Filtered), CriterionString]),
+    io:format ("~p~n", anagrams (B, Filtered, []));
 main ([CriterionString|Crap]) ->
     io:format ("(ignoring ~p ...)", [Crap]),
     main ([CriterionString]).
