22300261a5eb9bf55214cc67f759c9dc95704bc8
eric.hanchrow <eric.hanchrow@ce75a14a-3c2d-0410-9aa1-cbafc6a0a5bf>
2008-06-23 20:25:40 +0000

    do lower-casing earlier.
    git-svn-id: http://polyglot-anagrams.googlecode.com/svn@226 ce75a14a-3c2d-0410-9aa1-cbafc6a0a5bf

--- trunk/erlang/bag.erl
+++ trunk/erlang/bag.erl
@@ -3,9 +3,8 @@
 -define (Primes, {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101}).
 
 toprime (L) ->
-    Lower = string:to_lower (L),
-    if (Lower >= $a) and (Lower =< $z) ->
-            element (Lower - $a + 1, ?Primes);
+    if (L >= $a) and (L =< $z) ->
+            element (L - $a + 1, ?Primes);
        true ->
             1
     end.
@@ -15,7 +14,7 @@ bag (Letters) ->
                          toprime (Letter) * Result
                  end,
                  1,
-                 Letters).
+                 string:to_lower (Letters)).
 
 subtract (Top, Bottom) ->
     Rem  = Top rem Bottom,
