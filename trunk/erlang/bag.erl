bd48ff05f92eb6132e1e1e2d3e1668d62e849a39
eric.hanchrow <eric.hanchrow@ce75a14a-3c2d-0410-9aa1-cbafc6a0a5bf>
2008-06-23 20:16:03 +0000

    use built-in "string:to_lower", which I didn't know about until just now.
    git-svn-id: http://polyglot-anagrams.googlecode.com/svn@225 ce75a14a-3c2d-0410-9aa1-cbafc6a0a5bf

--- trunk/erlang/bag.erl
+++ trunk/erlang/bag.erl
@@ -2,14 +2,8 @@
 -export ([bag/1, subtract/2]).
 -define (Primes, {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101}).
 
-lowercase (L) when (L >= $A) and (L =< $Z) ->
-    L - $A + $a;
-
-lowercase (L) ->
-    L.
-
 toprime (L) ->
-    Lower = lowercase (L),
+    Lower = string:to_lower (L),
     if (Lower >= $a) and (Lower =< $z) ->
             element (Lower - $a + 1, ?Primes);
        true ->
