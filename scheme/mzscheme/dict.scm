4757d3dfbad9903e81ac2d574bfadfcc7718d853
Eric Hanchrow <eric.hanchrow@gmail.com>
2008-12-10 21:07:25 -0800

    some tests

--- scheme/mzscheme/dict.scm
+++ scheme/mzscheme/dict.scm
@@ -1,5 +1,13 @@
+#! /bin/sh
+#| Hey Emacs, this is -*-scheme-*- code!
+#$Id: v4-script-template.ss 5748 2008-11-17 01:57:34Z erich $
+exec  mzscheme --require "$0" --main -- ${1+"$@"}
+|#
+
 #lang scheme
-(require "bag.scm")
+(require (except-in "bag.scm" main))
+(require (planet schematics/schemeunit:3)
+         (planet schematics/schemeunit:3/text-ui))
 
 (provide init)
 
@@ -67,3 +75,27 @@
     result)
 
   )
+
+(provide main)
+(define (main . args)
+  (exit
+   (run-tests
+    (test-suite
+     "yow"
+     (let ((d (make-hash)))
+       (adjoin-word! d "frotz")
+       (let ((alist (hash-map d cons)))
+         (check-equal? alist (list (cons  (bag "frotz")
+                                          (list "frotz")))))
+       (adjoin-word! d "zortf")
+       (let ((alist (hash-map d cons)))
+         (check-equal? (caar alist) (bag "frotz"))
+         (check-not-false (member "zortf" (cdar alist)))
+         (check-not-false (member "frotz" (cdar alist))))
+
+       (adjoin-word! d "plonk")
+       (let* ((alist (hash-map d cons))
+              (probe (assoc (bag "plonk" )
+                            alist)))
+         (check-equal? 2 (length alist))
+         (check-equal? probe (cons (bag "plonk") (list "plonk")))))))))
