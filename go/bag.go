81f854c8756b5b645210cc9b283f7a3f4f7bb372
Eric Hanchrow <erich@cozi.com>
2009-11-13 18:10:30 -0800

    hey, it compiles!

--- go/bag.go
+++ go/bag.go
@@ -1,7 +1,7 @@
 package bag
 
-import "fmt"
+var primes = []int{2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101};
 
-func Snot () {
-     fmt.Printf("hello, world, or Καλημέρα κόσμε; or こんにちは 世界\n")
-     }
+func letterToPrime(ch int) int {
+	return primes[ch - 'a'];
+}
