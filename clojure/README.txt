05600cd58ef9acc5d0030fea05c453258dc0a216
Eric Hanchrow <erich@cozi.com>
2009-12-05 07:15:28 -0800

    Playin' with Clojure

--- clojure/README.txt
+++ clojure/README.txt
@@ -1,1 +1,4 @@
-http://clojure.sourceforge.net/
+http://clojure.sourceforge.net/ ...
+... but http://java.ociweb.com/mark/clojure/article.html is a nice
+intro
+
--- clojure/yow.clj
+++ clojure/yow.clj
@@ -1,6 +1,31 @@
-;; I dunno.
-(use 'clojure.contrib.str-utils)
+;; -*-lisp-*-
 
-(def dict-fn (str (System/getProperty "user.home") "/doodles/anagrams/words"))
+(use 'clojure.contrib.str-utils2)
+(use 'clojure.set)
+
+(defn contains-vowel? [w]
+  (some #(clojure.contrib.str-utils2/contains? w %)
+         (list "a" "e" "i" "o" "u" "y")))
+
+(defn word-acceptable? [w]
+  (let [w  (clojure.contrib.str-utils2/lower-case w)]
+       (and
+        (contains-vowel? w)
+        (or (= w "i")
+            (= w "a")
+            (< 1 (count w))
+            ))))
+
+(def dict-fn (str (System/getProperty "user.home") "/stray-doodles/anagrams/words.utf8"))
 (def in (-> dict-fn java.io.FileReader. java.io.BufferedReader. ))
-(def all-english-words  (re-split #"\n" (slurp dict-fn)))
+(def all-english-words  (filter word-acceptable? (re-split #"\n" (slurp dict-fn))))
+
+(def primes [2 3 5 7 11 13 17 19 23 29 31 37 41 43 47 53 59 61 67 71 73 79 83 89 97 101])
+(defn bag [w]
+  (apply * (map #(get primes (- (int %) (int \a)) 1) w)))
+
+(def dict
+     (let [h (hash-map)]
+          (map (fn [word]
+                   (update-in h (list word) #(clojure.set/union #{(bag word)} %)))
+               all-english-words)))
