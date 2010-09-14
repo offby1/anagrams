8a2d28d80358b1a96fbbb996ab134e1df009eb7c
Eric Hanchrow <eric.hanchrow@gmail.com>
2010-09-14 09:07:31 -0700

    Tart it up for no good reason

--- scheme/guile/go.scm
+++ scheme/guile/go.scm
@@ -1,8 +1,14 @@
 #!/bin/sh
-GUILE_LOAD_PATH=.
+here=$(cd $(dirname $0); pwd)
+GUILE_LOAD_PATH="$here"
 export GUILE_LOAD_PATH
 GUILE_WARN_DEPRECATED=detailed
 export GUILE_WARN_DEPRECATED
+
+if [ -z "$1" ];
+then
+    exec "$here/$0" "Frotz Plotz"
+fi
 
 exec guile -s $0 ${1+"$@"}
 !#
