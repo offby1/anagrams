9c103b941ad9269a32e8115016364bf1d9a43fe3
Eric Hanchrow <eric.hanchrow@gmail.com>
2011-03-26 12:28:59 -0700

    Add __slots__ in the hope it'll speed things up.

--- py3k/bag.py
+++ py3k/bag.py
@@ -7,6 +7,8 @@ import unittest
 class Bag(object):
     _primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101]
 
+    __slots__ = 'num'
+    
     def __init__(self, str):
         num = 1
 
