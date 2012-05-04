3b095e3ad80cfaa41086c40793e0195897c2eb3d
Eric Hanchrow <eric.hanchrow@gmail.com>
2012-05-03 20:51:27 -0700

    Some JVM options that might make life easier.
    From a guy at the SeaJure meeting.

--- clojure/project.clj
+++ clojure/project.clj
@@ -1,4 +1,6 @@
 (defproject anagrams "1.0.0-SNAPSHOT"
+  :jvm-opts ["-XX:GCTimeRatio=9"
+             "-Xmx1g"]
   :description "Display anagrams of a phrase"
   :dependencies [[org.clojure/clojure "1.3.0"]
                  ]
