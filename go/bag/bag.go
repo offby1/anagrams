b8294740823ff0babcc548983f4e822e77cf3c4e
Eric Hanchrow <eric.hanchrow@gmail.com>
2013-01-05 15:43:30 -0800

    Starting "Subtract"

--- go/bag/bag.go
+++ go/bag/bag.go
@@ -5,6 +5,7 @@ import "strings"
 var primes = []int{2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101}
 
 type Bag int
+type ErrCannotSubtract string
 
 func LetterToPrime(ch int) int {
 	index := ch - 'a'
@@ -22,4 +23,18 @@ func WordToBag(w string) Bag {
 		product *= LetterToPrime(int(c))
 	}
 	return Bag(product)
+}
+
+func (e ErrCannotSubtract) Error() string {
+	return "sorry, dude"
+}
+
+var TheOnlyError ErrCannotSubtract
+
+func Subtract(minuend, subtrahend Bag) (Bag, error) {
+	r := int(minuend) % int(subtrahend)
+	if r == 0 {
+		return Bag(int(minuend) / int(subtrahend)), nil
+	}
+	return Bag(1), &TheOnlyError
 }
--- go/bag/bag_test.go
+++ go/bag/bag_test.go
@@ -15,19 +15,21 @@ func TestLetterToPrime(t *testing.T) {
 	}
 }
 
-type TestCase struct {
-	Input          string
-	ExpectedOutput Bag
-}
-
 func TestWordsToBags(t *testing.T) {
+	type TestCase struct {
+		Input          string
+		ExpectedOutput Bag
+	}
+
 	var cases = []TestCase{
 		TestCase{"", 1},
 		TestCase{"cat", 710},
+		TestCase{" Cat ! ... ", 710},
 		TestCase{"CAT", 710},
 		TestCase{"tac", 710},
 		TestCase{"acat", 1420},
 	}
+
 	for _, c := range cases {
 		actual := WordToBag(c.Input)
 		if actual != c.ExpectedOutput {
@@ -35,6 +37,32 @@ func TestWordsToBags(t *testing.T) {
 				c.Input,
 				actual,
 				c.ExpectedOutput)
+		}
+	}
+}
+
+func TestSubtraction(t *testing.T) {
+	type TestCase struct {
+		Minuend            string
+		Subtrahend         string
+		ExpectedDifference Bag
+		ExpectedError      bool
+	}
+
+	var cases = []TestCase{
+		TestCase{"", "", 1, false},
+		TestCase{"a", "", WordToBag("a"), false},
+		TestCase{"", "a", Bag(1), true},
+	}
+
+	for _, c := range cases {
+		diff, err := Subtract(WordToBag(c.Minuend), WordToBag(c.Subtrahend))
+		if (err == nil) == c.ExpectedError || diff != c.ExpectedDifference {
+			t.Errorf("For '%s' - '%s', got %d, %s but expected %d, %s",
+				c.Minuend,
+				c.Subtrahend,
+				diff, err,
+				c.ExpectedDifference, c.ExpectedError)
 		}
 	}
 }
