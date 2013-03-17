2a34c1a34500be583587a23c317860894b81a42b
Eric Hanchrow <eric.hanchrow@gmail.com>
2013-03-17 14:25:45 -0700

    Start thining about decoding the bag gobs.

--- go/src/anagrams/anagrams.go
+++ go/src/anagrams/anagrams.go
@@ -1,22 +1,26 @@
 package anagrams
 
 import (
+	"log"
 	"math/big"
 )
 
 func Anagrams(d DictSlice, bag *big.Int) []string {
 	result := []string{}
 
-	/* 
-		for index, entry := range d {
-			// src/anagrams/anagrams.go:11: cannot use entry.bag (type string) as type *big.Int in function argument
-			// smaller_bag, error := Subtract(entry.bag, bag)
+	for _, entry := range d {
+		entry_bag := big.NewInt(0)
+		entry_bag.GobDecode([]byte(entry.bag_gob))
 
-			// try to subtract our bag from this entry's bag; save result in "smaller bag".
-			// if we cannot, just skip this entry.
-			// if smaller bag is empty, "listify" this entry's words, and append that list to the result.
-			// otherwise, make a smaller dict by filtering out entries, then recursively call ourselves with that smaller dict and the smaller bag; make a "cross-product" of this entry's words with the results of that recursive call.
-		}
-	*/
+		log.Fatalf("Decoded %v into %v", entry.bag_gob, entry_bag)
+
+		// smaller_bag, error := Subtract(entry.bag, bag)
+
+		// try to subtract our bag from this entry's bag; save result in "smaller bag".
+		// if we cannot, just skip this entry.
+		// if smaller bag is empty, "listify" this entry's words, and append that list to the result.
+		// otherwise, make a smaller dict by filtering out entries, then recursively call ourselves with that smaller dict and the smaller bag; make a "cross-product" of this entry's words with the results of that recursive call.
+	}
+
 	return result
 }
--- go/src/main/main.go
+++ go/src/main/main.go
@@ -15,20 +15,22 @@ func main() {
 			anagrams.WordToBag(w))
 	}
 
-	data, error := anagrams.SnarfDict("/usr/share/dict/words")
+	dictslice, error := anagrams.SnarfDict("/usr/share/dict/words")
 
 	if error != nil {
 		log.Fatal(error)
-	} else {
-		fmt.Printf("Number of somethings in the dictionary: %v\n", len (data))
-		for index, entry := range data {
-			fmt.Printf("Entries for '%d': %v\n",
-				index,
-				entry)
+	}
 
-			if index > 10 {
-				break
-			}
+	fmt.Printf("Number of somethings in the dictionary: %v\n", len (dictslice))
+	for index, entry := range dictslice {
+		fmt.Printf("Entries for '%d': %v\n",
+			index,
+			entry)
+
+		if index > 10 {
+			break
 		}
 	}
+
+	anagrams.Anagrams (dictslice, anagrams.WordToBag ("dog"))
 }
