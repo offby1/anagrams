acddedfa03eccda5d1ac7dd705f2c008b5ec03cc
Eric Hanchrow <eric.hanchrow@gmail.com>
2013-03-16 19:13:19 -0700

    Beginnings of reading a file.
    GOPATH=/home/erich/anagrams/go/ go install main && ./bin/main

---
+++ go/src/anagrams/dict.go
@@ -0,0 +1,26 @@
+package anagrams
+
+import (
+	"fmt"
+	"bufio"
+	"log"
+	"os"
+)
+
+// Open /usr/share/dict/words
+// read each word; return a map from bags to a set of words
+
+func SnarfDict() ([]byte, error) {
+	fmt.Printf("Ahoy?\n")
+	file, err := os.Open ("/usr/share/dict/words")
+	if err != nil {
+		log.Fatal (err)
+	}
+
+	reader := bufio.NewReader (file)
+	_, _ = reader.ReadBytes ('\n');
+	_, _ = reader.ReadBytes ('\n');
+	_, _ = reader.ReadBytes ('\n');
+	_, _ = reader.ReadBytes ('\n');
+	return reader.ReadBytes ('\n');
+}
--- go/src/main/main.go
+++ go/src/main/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"anagrams"
 	"fmt"
+	"log"
 )
 
 func main() {
@@ -12,5 +13,13 @@ func main() {
 		fmt.Printf("%s:\t%d\n",
 			w,
 			anagrams.WordToBag(w))
+	}
+
+	data, error := anagrams.SnarfDict()
+
+	if error != nil {
+		log.Fatal(error)
+	} else {
+		fmt.Printf("I dare to hope it worked.  %s\n", data)
 	}
 }
