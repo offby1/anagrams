ad9309b5fa8cf36aebbbfa9948916903d05259ac
Eric Hanchrow <eric.hanchrow@gmail.com>
2013-03-17 19:28:06 -0700

    Enable profiling as per http://blog.golang.org/2011/06/profiling-go-programs.html

--- go/src/main/main.go
+++ go/src/main/main.go
@@ -2,12 +2,26 @@ package main
 
 import (
 	"anagrams"
+	"flag"
 	"fmt"
 	"log"
 	"os"
+	"runtime/pprof"
 )
 
+var cpuprofile = flag.String("cpuprofile", "", "write cpu profile to file")
+
 func main() {
+    flag.Parse()
+    if *cpuprofile != "" {
+        f, err := os.Create(*cpuprofile)
+        if err != nil {
+            log.Fatal(err)
+        }
+        pprof.StartCPUProfile(f)
+        defer pprof.StopCPUProfile()
+    }
+
 	dictslice, error := anagrams.SnarfDict("/usr/share/dict/words")
 
 	if error != nil {
