e72f99518b0fe43b1323c75ab72a9eda0118f2de
Eric Hanchrow <eric.hanchrow@gmail.com>
2015-10-06 16:56:00 -0700

    Fix some warnings and errors
    This is perl6 version 2015.07.2 built on MoarVM version 2015.07

--- perl/perl6/bag.pm
+++ perl/perl6/bag.pm
@@ -1,14 +1,14 @@
 use v6;
 use Test;
 
-module bag;
+unit module bag;
 
 class Bag {
   method empty { self.letters.empty }
     has $.letters;
 };
 
-our multi sub infix:<eqv>(Bag $a, Bag $b)
+multi sub infix:<eqv>(Bag $a, Bag $b)
   {
     say "hey $a $b";
     $a.letters eqv $b.letters;
@@ -25,4 +25,4 @@ sub bag_from_letters($letters) is export {
 }
 
 ok(bag_from_letters('abcdeaa') eqv bag_from_letters('aaabcde'));
-is_deeply(bag_from_letters('abcdeaa').letters, bag_from_letters('aaabcde').letters);
+is-deeply(bag_from_letters('abcdeaa').letters, bag_from_letters('aaabcde').letters);
