700a772430c4fb766671cb2238e28445fd3b06e0
Eric Hanchrow <eric.hanchrow@gmail.com>
2015-10-15 12:04:16 -0700

    README and sbcl-main

---
+++ cl/sbcl-main.lisp
@@ -0,0 +1,23 @@
+(defpackage anagrams
+  (:use :common-lisp))
+(in-package :anagrams)
+(declaim (optimize speed))
+
+(mapcar #'(lambda (basename)
+            (load (concatenate 'string basename
+                               #+cmu  ".x86f"
+                               #+sbcl ".fasl"
+                               #+clisp ".fas"
+                               )))
+        `("numeric-bag"
+          ;"bag"
+          "dict"
+          "anagrams"))
+
+
+
+(anagrams  "Piffle")
+(format t "Done!~%" )
+
+(in-package :common-lisp-user)
+(quit)
