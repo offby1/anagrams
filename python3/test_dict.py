7047e21c68439da007ba23bae89aa0bb4bf4041d
Eric Hanchrow <eric.hanchrow@gmail.com>
2017-05-14 09:18:04 -0700

    comment

--- python3/test_dict.py
+++ python3/test_dict.py
@@ -22,6 +22,12 @@ def test_word_acceptable():
     assert (word_acceptable("dog"))
     assert not (word_acceptable("C3PO"))
 
+    # These tests check that I'm using the word list that I expect --
+    # not that there'd be anything terribly wrong with using some
+    # other list, but I compare the different implementations of this
+    # program based on the time they take to run, and that time is
+    # greatly affected by the choice of word list ... so I use the
+    # same word list for each implementation.
     d = snarf_dictionary(os.path.join(default_dict_name))
     assert (66965 == len(d))
     assert (72794 == sum(len(words) for words in d.values()))
